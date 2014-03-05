1d0259c919d6efe7e121bab05f3647cf696fbb31
Dave <dkoruba@gmail.com>
2014-03-05 13:11:00 +0100

    asdf

---
+++ git_config.bat
@@ -0,0 +1,7 @@
+git config --global user.name "Dave"
+git config --global user.email "dkoruba@gmail.com"
+git config --global core.editor gvim.exe
+git config --global merge.tool vimdiff
+git config --global diff.tool gvim.exe
+git config --global credential.helper "cache --timeout=3600"
+
