62b7324f6cd7384396acedd63674f2717aa95f49
gringo <dkoruba@gmail.com>
2014-04-09 11:01:54 +0200

    more git aliases

--- PowerShell/git-alias.ps1
+++ PowerShell/git-alias.ps1
@@ -1,1 +1,17 @@
 function gs { git status }
+
+function git-commit ($message){
+    git commit -m $message
+}
+
+Set-Alias -Name gic -Value git-commit
+
+function git-reset { git reset HEAD --hard }
+
+Set-Alias -Name gir -Value git-reset
+
+function git-push { git push -u origin master }
+Set-alias -name gip -Value git-push
+
+function git-add { git add -A }
+Set-alias -name gia -Value git-add
