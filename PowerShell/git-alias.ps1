8f586534635e7df6823b174a8270b643c6fdeffe
dkoruba <dawid.koruba@thecogworks.co.uk>
2016-05-11 13:55:16 +0200

    fetch all branches

--- PowerShell/git-alias.ps1
+++ PowerShell/git-alias.ps1
@@ -3,18 +3,21 @@ function gs { git status }
 function git-commit ($message){
     git commit -m $message
 }
-
 Set-Alias -Name gic -Value git-commit
 
 function git-reset { git reset HEAD --hard }
-
 Set-Alias -Name gir -Value git-reset
 
-function git-push { git push -u origin master }
-Set-alias -name gip -Value git-push
+function git-push([string] $branch) { git push -u origin $branch }
+Set-alias -name gpo -Value git-push
+
+
 
 function git-add { git add -A :/ }
 Set-alias -name gia -Value git-add
+
+function git-grep ([string] $pattern) { git grep $pattern }
+Set-Alias -Name gg -Value git-grep
 
 #Set-alias -Name giat -Value git add -n :/
 
@@ -37,3 +40,35 @@ function global:prompt {
     Write-Host("> ")
     return ""
 }
+
+
+$global:GitAllSettings = New-Object PSObject -Property @{
+	FolderForegroundColor       = [ConsoleColor]::Cyan
+}
+
+function git-all()
+{
+	$s = $global:GitAllSettings
+	dir -r -i .git -fo | % {
+		pushd $_.fullname
+		cd ..
+		write-host -fore $s.FolderForegroundColor (get-location).Path
+		git-fetchall
+		popd
+	}
+}
+
+function git-fetchall()
+{
+	$remotes = git remote
+	if($remotes){
+		$remotes | foreach {
+			Write-Host 'Fetching from' $_
+			git fetch $_ --all
+		}
+	}else{
+		Write-Host 'No remotes for this repository'
+	}
+	git status
+}
+
