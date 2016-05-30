373235157f3056b99a2daaed57df5d3b476cbb19
dkoruba <dawid.koruba@thecogworks.co.uk>
2016-05-30 11:00:51 +0200

    Save at 2016-05-30 11:00:51Z

--- PowerShell/config/ps-read-line-config.ps1
+++ PowerShell/config/ps-read-line-config.ps1
@@ -1,5 +1,10 @@
 # PSReadline
 #
+#
+Set-PSReadlineOption -HistoryNoDuplicates
+Set-PSReadlineOption -EditMode Vi
+Set-PSReadlineOption -ViModeIndicator Cursor
 
 Set-PSReadlineKeyHandler -Key Ctrl+Tab -Function PossibleCompletions
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
