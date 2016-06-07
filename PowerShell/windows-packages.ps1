f7bdc98d0b3c6691785ebe0b140e82410a54f25b
dkoruba <dawid.koruba@thecogworks.com>
2016-06-07 17:47:13 +0200

    windows packaget to install on clean Windows

---
+++ PowerShell/windows-packages.ps1
@@ -0,0 +1,56 @@
+
+Get-PackageProvider -name chocolatey
+
+Find-Package -Name vlc
+
+get-package "7-Zip 15.12 (x64)"
+
+Find-Package -Name 7zip
+
+Find-Package -Name googlechrome | Select -First 1 | Format-List
+
+
+choco install nodejs.install
+choco install nodejs
+
+choco install notepadplusplus.install
+
+choco install 7zip.install
+
+choco install git.install
+
+choco install notepadplusplus
+
+choco install vlc
+
+choco install skype
+
+choco install powershell
+
+choco install putty
+
+choco install dropbox
+
+choco install curl
+
+choco install python
+
+choco install paint.net
+
+choco install fiddler
+
+choco install sourcetree
+
+choco install foxitreader
+
+choco install tortoisegit
+
+choco install googledrive
+
+choco install linqpad4
+
+choco install slack
+
+choco install qbittorrent
+
+choco install foobar2000
